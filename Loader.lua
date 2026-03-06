51bf05d3796b1f47b278aa8240b76b8bcdc48c1b
DLC1of1 <rrichmy@gmail.com>
2026-03-06 22:46:30 +0700

    Add Loader.lua for script loading functionality

---
+++ Loader.lua
@@ -0,0 +1,6 @@
+--[[
+	Credit: Hermanos-Dev, Dont Delete This.
+]]--
+
+getgenv().script_mode = "PVP" -- PVP, FARM
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hermanos-dev/hermanos-hub/refs/heads/main/Loader.lua"))()
